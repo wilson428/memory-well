ee5d7b65f39b92fdcbb8f3a098cb465b8c7ac957
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2016-08-08 01:19:57 -0400

    Finish 4/5 story wells

---
+++ app/controllers/story_wells_controller.rb
@@ -0,0 +1,13 @@
+class StoryWellsController < ApplicationController
+  def graham_newton_small
+  end
+
+  def mary_daly
+  end
+
+  def charline_tyler
+  end
+
+  def liz_ross
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
 
   root 'home#welcome'
 
+  # Static home pages
   get 'home/for_families'
-
   get 'home/for_caregivers'
-
   get 'home/samples'
-
   get 'home/about_us'
-
   get 'home/get_started'
+
+  # Story well pages
+  get 'story-wells/graham-newton-small', to: 'story_wells#graham_newton_small', as: 'graham_newton_small'
+  get 'stpaulshouse/mary-daly',          to: 'story_wells#mary_daly',           as: 'mary_daly'
+  get 'stpaulshouse/charline-tyler',     to: 'story_wells#charline_tyler',      as: 'charline_tyler'
+  get 'stpaulshouse/liz-ross',           to: 'story_wells#liz_ross',            as: 'liz_ross'
 
 end
